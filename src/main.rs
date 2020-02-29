64c1e622ee569a84f7b58bf03ae6f17a4068d300
Reconcyl <22424430+Reconcyl@users.noreply.github.com>
2020-02-29 09:44:54 -0800

    add notes and additional RNG util

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
+use std::collections::HashMap;
+
 mod utils;
 
 fn main() {
     let mut rng = rand::thread_rng();
-    let mut weights = [0.1, 0.7, 0.2];
-    let mut choices = [0, 0, 0];
+    let mut c = [0u32; 256];
+
     for _ in 0..1_000_000 {
-        let idx = utils::weighted_choice(&mut rng, &weights);
-        choices[idx] += 1;
+        c[utils::small_positive(&mut rng) as usize] += 1;
     }
-    println!("{:?}", choices);
+
+    println!("{:?}", c.iter().enumerate().filter(|&(_, &n)| n != 0).collect::<Vec<_>>());
 }
--- src/utils.rs
+++ src/utils.rs
@@ -1,6 +1,8 @@
 use rand::Rng;
-use rand::distributions::Open01;
+use rand::distributions::{Open01, Uniform};
 
+/// Choose an integer `n` from the range 0..weights.len() with probability
+/// proportional to `weights[n]`.
 pub fn weighted_choice<R: Rng>(rng: &mut R, weights: &[f64]) -> usize {
     let sum = weights.iter().sum::<f64>();
     let val = sum * rng.sample::<f64, Open01>(Open01);
@@ -10,4 +12,19 @@ pub fn weighted_choice<R: Rng>(rng: &mut R, weights: &[f64]) -> usize {
         i += 1;
     }
     return i;
+}
+
+/// Return a small positive number. This can be anywhere from 0 to 45 but in practice
+/// will almost always be less than 15.
+pub fn small_positive<R: Rng>(rng: &mut R) -> u8 {
+    let dist = Uniform::new_inclusive(-2, 2);
+    let mut sum = 0i8;
+    for _ in 0..15 {
+        sum += rng.sample(dist);
+    }
+    if sum == 0 {
+        1
+    } else {
+        sum.abs() as u8
+    }
 }
